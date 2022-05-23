99aa2c582d238a0995a040b66ca4d0c76c28bd30
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-05-23 22:23:20 +0700

    Update pemburujanda.sh

--- pemburujanda.sh
+++ pemburujanda.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.66.138.250:5896
-socks5_username = ingfoingfo
-socks5_password = maszZeehh
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -23,14 +23,14 @@ sleep .2
 echo " "
 echo " "
 
-echo "******************************************************************"
+echo ""
 
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-echo "******************************************************************"
+echo ""
 
 echo " "
 echo " "
